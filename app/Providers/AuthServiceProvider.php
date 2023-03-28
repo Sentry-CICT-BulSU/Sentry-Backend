d2d832f3be35f08d08a3d9df22c05a5f51b4a52b
rhonstratos <kizunaai20@gmail.com>
2023-03-28 14:47:59 +0800

    fix authorization request view

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Passport\{
+    AuthCode,
+    Client,
+    PersonalAccessClient,
+    RefreshToken,
+    Token
+};
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,9 +30,58 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
+        // Passport::hashClientSecrets();
+
+        Passport::tokensExpireIn(now()->addHours(24));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addDays(30));
+
+        Passport::useTokenModel(Token::class);
+        Passport::useRefreshTokenModel(RefreshToken::class);
+        Passport::useAuthCodeModel(AuthCode::class);
+        Passport::useClientModel(Client::class);
+        Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
+
+
+        /* Passport::tokensCan([
+            'create-keys' => 'Create Room Keys',
+            'read-keys' => 'Check Room Keys',
+            'update-keys' => 'Update Room Keys',
+            'delete-keys' => 'Delete Room Keys',
+
+            'create-attendance' => 'Create Attendance Schedule',
+            'read-attendance' => 'Check Attendance Schedule',
+            'update-attendance' => 'Update Attendance Schedule',
+            'delete-attendance' => 'Delete Attendance Schedule',
+
+            'create-logs' => 'Create Logs',
+            'read-logs' => 'Check Logs',
+            'update-logs' => 'Update Logs',
+            'delete-logs' => 'Delete Logs',
+
+            'admin' => 'Access all app features'
+        ]);
+
+        Passport::setDefaultScope([
+
+            // 'create-keys',
+            'read-keys',
+            // 'update-keys',
+            // 'delete-keys',
+
+            // 'create-attendance',
+            'read-attendance',
+            // 'update-attendance',
+            // 'delete-attendance',
+
+            // 'create-logs',
+            'read-logs',
+            // 'update-logs',
+            // 'delete-logs',
+        ]); */
 
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
         //
