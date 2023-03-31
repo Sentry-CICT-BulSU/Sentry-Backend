e33d23459c4002eba1703f6a7936f288b965e4c7
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-03-31 22:47:28 +0800

    update tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -18,11 +19,18 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            // password
             'remember_token' => Str::random(10),
+            'profile_img' => null,
+            'type' => fake()->randomElement(User::TYPES),
+            'position' => fake()->randomElement(['Instructor I']),
+            'college' => 'College of Information and Communcations Technology',
+            'contact' => fake()->phoneNumber(),
         ];
     }
 
@@ -31,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
