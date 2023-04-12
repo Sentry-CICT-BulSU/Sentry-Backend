564653921d4e72198c9beb33304299e9f467e092
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-12 21:51:14 +0800

    vite env

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    server: {
-        hmr: {
-            host: import.meta.env.APP_URL_FLAT,
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+    return defineConfig({
+        server: {
+            hmr: {
+                host: process.env.VITE_APP_URL_FLAT,
+            },
         },
-    },
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
-});
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    });
+
+}
