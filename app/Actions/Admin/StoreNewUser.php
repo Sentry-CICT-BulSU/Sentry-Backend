e3a426132b0fcf6f2c2217df85c7de0449a49a59
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-14 02:23:32 +0800

    fix user store profile img error

--- app/Actions/Admin/StoreNewUser.php
+++ app/Actions/Admin/StoreNewUser.php
@@ -3,19 +3,19 @@ namespace App\Actions\Admin;
 
 use App\Http\Requests\Api\Users\StoreUser;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class StoreNewUser
 {
     public function handle(StoreUser $request): User
     {
-        $fileName = $request->file('profile_img')->storePublicly('profile_imgs');
-        if (!$fileName) {
-            throw new \Exception('Failed to upload profile image', 500);
-        }
         $data = $request->validated();
-        $data['profile_img'] = $fileName;
+        if ($$request->file('profile_img')) {
+            $fileName = $request->file('profile_img')->storePublicly('profile_imgs');
+            if (!$fileName) {
+                throw new \Exception('Failed to upload profile image', 500);
+            }
+            $data['profile_img'] = $fileName;
+        }
         return User::create($data);
     }
 }
