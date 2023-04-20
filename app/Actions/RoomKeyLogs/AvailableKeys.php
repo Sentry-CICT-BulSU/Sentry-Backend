fd05520076b3b4c56a521864a69ae58bb9bb037a
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-04-20 22:47:06 +0800

    fix ZeroDivision error

--- app/Actions/RoomKeyLogs/AvailableKeys.php
+++ app/Actions/RoomKeyLogs/AvailableKeys.php
@@ -15,6 +15,15 @@ class AvailableKeys
         $unavailableKeys = sizeof($keys->filter(fn($log) => $log->status !== RoomKeys::STATUSES[RoomKeys::AVAILABLE])->toArray());
         $availableKeys = sizeof($keys->filter(fn($log) => $log->status === RoomKeys::STATUSES[RoomKeys::AVAILABLE])->toArray());
         $total = $keys->count();
+        if ($total === 0) {
+            return [
+                'availabe' => 0,
+                'availabe_percentage' => 0,
+                'unavailable' => 0,
+                'unavailable_percentage' => 0,
+                'total_keys' => 0,
+            ];
+        }
         $decrease = (($total - $unavailableKeys) / $total) * 100;
         $increase = ($unavailableKeys / $total) * 100;
 
