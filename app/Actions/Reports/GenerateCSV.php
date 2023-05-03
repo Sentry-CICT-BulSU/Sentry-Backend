72b958619a4b6a98001e3ad80faa2fbe2f89a888
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-04 03:10:32 +0800

    preview report

--- app/Actions/Reports/GenerateCSV.php
+++ app/Actions/Reports/GenerateCSV.php
@@ -7,26 +7,34 @@ use App\Models\Attendances;
 use App\Models\RoomKeyLogs;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Maatwebsite\Excel\Excel;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class GenerateCSV
 {
     private array $headings;
-    public function handle(Request $request): \Illuminate\Http\Response|\Symfony\Component\HttpFoundation\BinaryFileResponse
-    {
+    public function handle(
+        Request $request
+    ): mixed {
         $report = (new CSVExport($this->query($request), $this->headings));
-        return match (config('excel.defaults.export_type')) {
-            'xlsx' => $report->download(
-                'report.xlsx',
-                Excel::XLSX,
-                ['Content-Type' => 'text/xlsx']
-            ),
-            default => $report->download(
-                'report.csv',
-                Excel::CSV,
-                ['Content-Type' => 'text/csv']
-            ),
-        };
+        return $request->has('preview')
+            ? [
+                'data' => $this->query($request)->get()->take(10),
+                'type' => $request->get('type')
+            ]
+            : match (config('excel.defaults.export_type')) {
+                'xlsx' => $report->download(
+                    'report.xlsx',
+                    Excel::XLSX,
+                    ['Content-Type' => 'text/xlsx']
+                ),
+                default => $report->download(
+                    'report.csv',
+                    Excel::CSV,
+                    ['Content-Type' => 'text/csv']
+                ),
+            };
     }
     public function query(Request $request)
     {
