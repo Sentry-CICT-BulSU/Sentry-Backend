5fb2878f58fdb93c6c31e3dc25e31c70d73c78d5
Cramzzzberry <janroebantuan@gmail.com>
2023-05-04 15:19:52 +0800

    schedule query

--- app/Http/Controllers/Api/SchedulesController.php
+++ app/Http/Controllers/Api/SchedulesController.php
@@ -25,49 +25,7 @@ class SchedulesController extends Controller
 {
     public function index(Request $request, Query $query): JsonResponse
     {
-        $query->handle($request);
-        $schoolYear = Carbon::now()->year . '-' . Carbon::now()->addYear()->year;
-        $dayNameNow = Carbon::now()->dayName;
-        $schedule = Schedules::query()
-            ->with([
-                'section' => fn ($q) => $q->withTrashed(),
-                'room' => fn ($q) => $q
-                    ->when(
-                        ($request->has('rid') && $request->get('rid') === 'am'),
-                        fn ($rq) => $rq->where('id', $request->get('rid'))
-                    )->withTrashed(),
-                'adviser' => fn ($q) => $q->when(($request->has('fid')), fn ($qf) => $qf->where('id', $request->get('fid')))->withTrashed(),
-                'subject' => fn ($q) => $q->withTrashed(),
-                'semester' => fn ($q) => match (Auth::user()->type) {
-                    User::ADMIN => $q->where('academic_year', $schoolYear)->withTrashed(),
-                    default => $q->where('academic_year', $schoolYear)
-                },
-                'attendance' => fn ($q) => $q->withTrashed(),
-            ])
-            ->when(
-                ($request->has('q') && $request->get('q') === 'am'),
-                fn ($q) => $q //->whereBetween('time_start', [Carbon::parse('00:00:00')->toTimeString(), Carbon::parse('11:59:59')->toTimeString()])
-                    ->whereTime('time_start', '>=', Carbon::parse('00:00:00')->toTimeString())
-                    ->whereTime('time_end', '<=', Carbon::parse('11:59:59')->toTimeString())
-                    ->whereJsonContains('active_days', strtolower($dayNameNow))
-            )
-            ->when(
-                ($request->has('q') && $request->get('q') === 'pm'),
-                fn ($q) => $q //->whereBetween('time_start', [Carbon::parse('12:00:00')->toTimeString(), Carbon::parse('23:59:59')->toTimeString()])
-                    ->whereTime('time_start', '>=', Carbon::parse('12:00:00')->toTimeString())
-                    ->whereTime('time_end', '<=', Carbon::parse('23:59:59')->toTimeString())
-                    ->whereJsonContains('active_days', strtolower($dayNameNow))
-            )
-            ->when(
-                ($request->has('admin-dash') || !$request->has('q') && Auth::user()->type !== User::TYPES[User::ADMIN]),
-                fn ($q) => $q
-                    ->whereJsonContains('active_days', strtolower($dayNameNow))
-                    ->whereTime('time_start', '>=', Carbon::now()->toTimeString())
-                    ->whereTime('time_end', '<=', Carbon::now()->toTimeString())
-            )
-            ->orderBy('time_start')
-            ->orderBy('time_end')
-            // ->when($request->has('q') && $request->q === 'faculty', fn($q) => $q->where('', 'faculty'))
+        $schedule = $query->handle($request)
             ->paginate(15);
         return SchedulesResource::collection($schedule)->response();
     }
