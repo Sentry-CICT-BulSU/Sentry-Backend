6febd6f13018b97b08a6dcc771cb09c82e7c698f
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-04 10:46:03 +0800

    fix schedules query

--- app/Http/Controllers/Api/SchedulesController.php
+++ app/Http/Controllers/Api/SchedulesController.php
@@ -44,11 +44,15 @@ class SchedulesController extends Controller
             ])
             ->when(
                 ($request->has('q') && $request->get('q') === 'am'),
-                fn($q) => $q->whereBetween('time_start', [Carbon::parse('00:00:00')->toTimeString(), Carbon::parse('11:59:59')->toTimeString()])
+                fn($q) => $q //->whereBetween('time_start', [Carbon::parse('00:00:00')->toTimeString(), Carbon::parse('11:59:59')->toTimeString()])
+                    ->whereTime('time_start', '>=', Carbon::parse('00:00:00')->toTimeString())
+                    ->whereTime('time_end', '<=', Carbon::parse('11:59:59')->toTimeString())
             )
             ->when(
                 ($request->has('q') && $request->get('q') === 'pm'),
-                fn($q) => $q->whereBetween('time_start', [Carbon::parse('12:00:00')->toTimeString(), Carbon::parse('23:59:59')->toTimeString()])
+                fn($q) => $q //->whereBetween('time_start', [Carbon::parse('12:00:00')->toTimeString(), Carbon::parse('23:59:59')->toTimeString()])
+                    ->whereTime('time_start', '>=', Carbon::parse('12:00:00')->toTimeString())
+                    ->whereTime('time_end', '<=', Carbon::parse('23:59:59')->toTimeString())
             )
             ->when(
                 (Auth::user()->type !== User::TYPES[User::ADMIN]),
