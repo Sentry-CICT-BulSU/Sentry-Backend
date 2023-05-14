afa87e55678f54ff39df7eb6d46d718d70b13702
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-14 17:10:10 +0800

    fix admin queries

--- app/Actions/Charts/AdminChart.php
+++ app/Actions/Charts/AdminChart.php
@@ -11,61 +11,50 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdminChart
 {
+    private static function query(array $datePeriod)
+    {
+        return Attendances::query()
+            ->select(DB::raw('DATE(created_at) as x'), DB::raw('count(*) as y'))
+            ->groupBy('x')
+            ->whereBetween('created_at', $datePeriod);
+    }
     public static function getChart(): JsonResponse
     {
         $datePeriod = [
-            Carbon::now()->subDays(7)->toDateString(),
-            Carbon::now()->toDateString(),
+            Carbon::now()->subDays(7),
+            Carbon::now(),
         ];
-        $dates = collect(CarbonPeriod::createFromArray($datePeriod)->map(fn($q) => $q->format('Y-m-d')));
-        $query = Attendances::query()
-            ->select(DB::raw('DATE(created_at) as date_time'), DB::raw('count(*) as count'))
-            ->groupBy('date_time')
-            ->whereBetween('created_at', $datePeriod);
-        $presentees = $query
+        $dates = collect(CarbonPeriod::createFromArray($datePeriod)->map(fn($q) => $q->toDateString()));
+
+        $presentees = self::query($datePeriod)
             ->where('status', Attendances::STATUSES[Attendances::PRESENT])
-            ->get()->toArray();
-        $absentees = $query
+            ->get()->map(function ($e) {
+                $e->x = Carbon::parse($e->x)->toFormattedDayDateString();
+                return $e;
+            });
+
+        $absentees = self::query($datePeriod)
             ->where('status', Attendances::STATUSES[Attendances::ABSENT])
-            ->get()->toArray();
-        $presentees_filtered = [];
-        $absentees_filtered = [];
+            ->get()->map(function ($e) {
+                $e->x = Carbon::parse($e->x)->toFormattedDayDateString();
+                return $e;
+            });
 
-        foreach ($dates as $k => $date) {
-            $presentees_filtered[$k]['x'] = $date;
-            $absentees_filtered[$k]['x'] = $date;
-
-            $presentFlat = array_map(
-                fn($q) => $q['count'],
-                array_filter(
-                    $presentees,
-                    fn($q) => $q['date_time'] === $date,
-                )
-            );
-            if (empty($presentFlat)) {
-                $presentees_filtered[$k]['y'] = 0;
-            } else {
-                $presentees_filtered[$k]['y'] = Arr::first($presentFlat);
-            }
-
-            $absentFlat = array_map(
-                fn($q) => $q['count'],
-                array_filter(
-                    $absentees,
-                    fn($q) => $q['date_time'] === $date,
-                )
-            );
-            if (empty($absentFlat)) {
-                $absentees_filtered[$k]['y'] = 0;
-            } else {
-                $absentees_filtered[$k]['y'] = Arr::first($absentFlat);
-            }
-        }
+        $present_array = $dates->map(function ($e) use ($presentees) {
+            $date = Carbon::parse($e)->format('D, M d');
+            $present = $presentees->firstWhere('x', $e);
+            return $present ? ['x' => $date, 'y' => $present->y] : ['x' => $date, 'y' => 0];
+        })->toArray();
+        $absent_array = $dates->map(function ($e) use ($absentees) {
+            $date = Carbon::parse($e)->format('D, M d');
+            $absent = $absentees->firstWhere('x', $e);
+            return $absent ? ['x' => $date, 'y' => $absent->y] : ['x' => $date, 'y' => 0];
+        })->toArray();
 
         return response()->json([
-            'presentees' => $presentees_filtered,
-            'absentees' => $absentees_filtered,
-            'period' => $dates
+            'presentees' => $present_array,
+            'absentees' => $absent_array,
+            'period' => $dates->map(fn($e) => Carbon::parse($e)->format('D, M d'))
         ]);
     }
 }
