a3ef2533d53c94041934d7c244c718ee22a51650
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-14 15:40:23 +0800

    system settings

---
+++ app/Actions/SystemSettings/StoreSettings.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Actions\SystemSettings;
+
+use App\Models\SystemSettings;
+use Illuminate\Http\Request;
+
+class StoreSettings
+{
+    public static function handle(Request $request)
+    {
+        if ($request->file('icon')) {
+            $fileName = $request->file('icon')->storePublicly('sys_settings');
+            if (!$fileName) {
+                throw new \Exception('Failed to upload icon image', 500);
+            }
+            $data['icon'] = $fileName;
+        }
+        $sys_settings = SystemSettings::first();
+        if (is_null($sys_settings)) {
+            return SystemSettings::create($data);
+        }
+        $sys_settings->update($data);
+        return $sys_settings;
+    }
+}
--- app/Http/Controllers/Api/SystemSettingsController.php
+++ app/Http/Controllers/Api/SystemSettingsController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\SystemSettings\StoreSettings;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\SystemSettingsResource;
 use App\Models\SystemSettings;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SystemSettingsController extends Controller
 {
     public function index(): SystemSettingsResource
     {
-        return new SystemSettingsResource(SystemSettings::latest()->first());
+        return new SystemSettingsResource(SystemSettings::first());
     }
-    public function store(Request $request): SystemSettingsResource
+    public function store(Request $request): SystemSettingsResource|JsonResponse
     {
         $data = $request->validate([
             'name' => ['nullable', 'bail', 'string', 'max:255'],
@@ -22,13 +24,17 @@ class SystemSettingsController extends Controller
             'icon' => ['nullable', 'bail', 'image'],
             'color' => ['nullable', 'bail', 'string', 'max:255']
         ]);
-        if ($request->file('icon')) {
-            $fileName = $request->file('icon')->storePublicly('sys_settings');
-            if (!$fileName) {
-                throw new \Exception('Failed to upload icon image', 500);
-            }
-            $data['icon'] = $fileName;
+        try {
+            DB::beginTransaction();
+            $sys_settings = StoreSettings::handle($request);
+            DB::commit();
+            return new SystemSettingsResource($sys_settings);
+        } catch (\Exception $err) {
+            DB::rollBack();
+            return response()->json(
+                ['message' => $err->getMessage()],
+                $err->getCode() ?? 500
+            );
         }
-        return new SystemSettingsResource(SystemSettings::create($data));
     }
 }
