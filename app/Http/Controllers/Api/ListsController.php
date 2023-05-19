431bf4afaf2ba680c4ab9d7b0a6edf8c240836e3
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-19 16:45:02 +0800

    fix errors

--- app/Http/Controllers/Api/ListsController.php
+++ app/Http/Controllers/Api/ListsController.php
@@ -36,30 +36,51 @@ class ListsController extends Controller
     public function faculty()
     {
         return response()->json(
-            User::query()->whereNotIn('type', [USER::ADMIN])
-                ->orderBy(['first_name', 'last_name'])
-                ->get(['id', 'first_name', 'last_name'])
+            User::query()
+                ->select(['id', 'first_name', 'last_name'])
+                ->whereNotIn('type', [USER::ADMIN])
+                ->orderBy('first_name')
+                ->orderBy('last_name')
+                ->get()
                 ->append('full_name')
         );
     }
     public function subject()
     {
-        return response()->json(Subjects::query()->orderBy(['title'])->get(['id', 'title']));
+        return response()->json(
+            Subjects::query()
+                ->select(['id', 'title'])
+                ->orderBy('title')
+                ->get()
+        );
     }
     public function room()
     {
-        return response()->json(Rooms::query()->orderBy(['name'])->get(['id', 'name']));
+        return response()->json(
+            Rooms::query()
+                ->select(['id', 'name'])
+                ->orderBy('name')
+                ->get()
+        );
     }
     public function section()
     {
-        return response()->json(Sections::query()->orderBy(['name'])->get(['id', 'name']));
+        return response()->json(
+            Sections::query()
+                ->select(['id', 'name'])
+                ->orderBy('name')
+                ->get()
+        );
     }
     public function semester()
     {
         return response()->json(
-            Semesters::query()->select([
-                'id', DB::raw('CONCAT(name,\' - \', academic_year) as name_acad_yr')
-            ])->orderBy(['name_acad_yr'])->get()
+            Semesters::query()
+                ->select([
+                    'id', DB::raw('CONCAT(name,\' - \', academic_year) as name_acad_yr')
+                ])
+                ->orderBy('name_acad_yr')
+                ->get()
         );
     }
 }
