679085803f16383632c4ab1e9625a26fae8e8ce5
rhonstratos <53313285+rhonstratos@users.noreply.github.com>
2023-05-19 16:35:49 +0800

    list orderby

--- app/Http/Controllers/Api/ListsController.php
+++ app/Http/Controllers/Api/ListsController.php
@@ -37,28 +37,29 @@ class ListsController extends Controller
     {
         return response()->json(
             User::query()->whereNotIn('type', [USER::ADMIN])
+                ->orderBy(['first_name', 'last_name'])
                 ->get(['id', 'first_name', 'last_name'])
                 ->append('full_name')
         );
     }
     public function subject()
     {
-        return response()->json(Subjects::query()->get(['id', 'title']));
+        return response()->json(Subjects::query()->orderBy(['title'])->get(['id', 'title']));
     }
     public function room()
     {
-        return response()->json(Rooms::query()->get(['id', 'name']));
+        return response()->json(Rooms::query()->orderBy(['name'])->get(['id', 'name']));
     }
     public function section()
     {
-        return response()->json(Sections::query()->get(['id', 'name']));
+        return response()->json(Sections::query()->orderBy(['name'])->get(['id', 'name']));
     }
     public function semester()
     {
         return response()->json(
             Semesters::query()->select([
                 'id', DB::raw('CONCAT(name,\' - \', academic_year) as name_acad_yr')
-            ])->get()
+            ])->orderBy(['name_acad_yr'])->get()
         );
     }
 }
